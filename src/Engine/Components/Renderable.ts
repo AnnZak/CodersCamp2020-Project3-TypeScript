2f7c53902551c2df660697c3f9493dc37390921b
Cidebur <mcibor96@gmail.com>
2021-02-13 22:58:35 +0100

    Colidable broke down to Velocity, Gravity and Position

--- src/Engine/Components/Renderable.ts
+++ src/Engine/Components/Renderable.ts
@@ -1,11 +1,10 @@
 import Component from "./Component";
-import { Vector } from "../Utils/vector.h";
 import { Shape } from "../Utils/shape.h";
 import { Size } from "../Utils/size.h";
 
 export default class Renderable extends Component {
 
-    constructor(public color: string = "#000", public shape: Shape, public size: Size, public texture?: string) {
+    constructor(public color: string = "white", public shape: Shape, public size: Size, public texture?: string) {
         super();
     }
 }
