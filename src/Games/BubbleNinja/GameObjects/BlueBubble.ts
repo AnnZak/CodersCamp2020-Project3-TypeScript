7be3dfbd1e171b0d6e5bdecdf1a8a8f4280e6820
AnnZak <zak.anna.94@gmail.com>
2021-02-15 22:53:26 +0100

    added new bubble texture

--- src/Games/BubbleNinja/GameObjects/BlueBubble.ts
+++ src/Games/BubbleNinja/GameObjects/BlueBubble.ts
@@ -4,8 +4,10 @@ import Renderable from "../../../Engine/Components/Renderable";
 
 export default class BlueBubble extends Bubble {
     constructor(position: Vector, velocity: Vector, radius: number = 50) {
-        super(position, velocity, "blue", 10, radius, false);
+
+        const bubble = new Image();
+        bubble.src = 'https://github.com/Cidebur/CodersCamp2020-Project3-TypeScript/blob/new-bubbles-texture/src/Games/BubbleNinja/img/babelek.png?raw=true';
+
+        super(position, velocity, "blue", 10, radius, false, bubble);
     }
 }
-
-
