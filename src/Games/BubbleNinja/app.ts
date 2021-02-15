af91551359bbea6e964d5f0f16aa9f7555990918
Aleksandra Zochowska <a.j.zochowska@gmail.com>
2021-02-15 22:57:08 +0100

    points showing after finished game

--- src/Games/BubbleNinja/app.ts
+++ src/Games/BubbleNinja/app.ts
@@ -5,20 +5,22 @@ const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const mouse = new Mouse(canvas);
 
 const playButton: HTMLButtonElement = document.getElementById('play-button') as HTMLButtonElement;
-const startel: HTMLElement = document.getElementById('content') as HTMLButtonElement;
+const contentDiv: HTMLElement = document.getElementById('content') as HTMLButtonElement;
+const cont: HTMLElement = document.getElementById('cont') as HTMLButtonElement;
 const username = document.getElementById('username') as HTMLInputElement;
 
 playButton.addEventListener("click", e => {
     const container: HTMLElement = document.querySelector(".container") as HTMLElement;
-    container.classList.add("content-not-visible");
+    container.classList.add("not-visible");
 
     const game = new Game(canvas, mouse, username.value);
     game.onGameEnd((points) => {
-        
-        console.log(points);
+        contentDiv.innerHTML = `${points} points!`;
+        cont.classList.remove("not-visible");
+        cont.classList.add("flex-container");
+        contentDiv.classList.remove("not-visible");
+        contentDiv.classList.add("content-visible");
     })
 
-    const el: HTMLElement = document.getElementById('content')!;
-    el.classList.add("content-visible");
     game.start();
 });
